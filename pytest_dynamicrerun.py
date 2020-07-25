573c760b2923467b833b9c3f7329d02d447262b4
Gleb Nikonorov <gleb.i.nikonorov@gmail.com>
2020-07-24 22:36:25 -0400

    initial idea for how to trigger reruns and use cron format

--- pytest_dynamicrerun.py
+++ pytest_dynamicrerun.py
@@ -1,19 +1,49 @@
-import pytest
+from _pytest.runner import runtestprotocol
+
+# from croniter import croniter - we'll use this to schedule reruns
+
+PLUGIN_NAME = "dynamicrerun"
+
+
+def _add_dynamic_rerun_flag(parser):
+    help_text = "Set the time to attempt a rerun in using a cron like format ( e.g.: 5 * * * * )"
+
+    group = parser.getgroup(PLUGIN_NAME)
+    group.addoption(
+        "--rerun_schedule",
+        action="store",
+        dest="rerun_schedule",
+        default="* * * * *",
+        help=help_text,
+    )
+
+    parser.addini("rerun_schedule", help_text)
 
 
 def pytest_addoption(parser):
-    group = parser.getgroup("dynamicrerun")
-    group.addoption(
-        "--foo",
-        action="store",
-        dest="dest_foo",
-        default="2020",
-        help='Set the value for the fixture "bar".',
-    )
-
-    parser.addini("HELLO", "Dummy pytest.ini setting")
+    _add_dynamic_rerun_flag(parser)
 
 
-@pytest.fixture
-def bar(request):
-    return request.config.option.dest_foo
+def pytest_runtest_protocol(item, nextitem):
+    item.ihook.pytest_runtest_logstart(nodeid=item.nodeid, location=item.location)
+    reports = runtestprotocol(item, nextitem=nextitem, log=False)
+    for report in reports:
+        if report.failed:
+            # failed test, store it and log as pytest would
+            # NOTE: we can store the failed items like this. Then in sesisonfinsh we can see if anything has failed
+            #       if something has failed then we can schedule a rerun. We can share session state as on line 44
+            item.ihook.pytest_runtest_logreport(report=report)
+            item.session.dynamicrerun = {"failed_item": None}
+            item.session.dynamicrerun["failed_item"] = item
+
+    item.ihook.pytest_runtest_logfinish(nodeid=item.nodeid, location=item.location)
+    return True
+
+
+# def pytest_sessionfinish(session, exitstatus):
+#    raise ValueError(session.dynamicrerun)
+
+# Consider providing a fixture too
+# @pytest.fixture
+# def bar(request):
+#    return request.config.option.dest_foo
