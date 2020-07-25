0055799f5e697b48fe6f8d318a75facbbdcdaf4b
Gleb Nikonorov <gleb.i.nikonorov@gmail.com>
2020-07-25 10:01:19 -0400

    Add rerun attempts flag

--- pytest_dynamicrerun.py
+++ pytest_dynamicrerun.py
@@ -1,49 +1,74 @@
 import time
-import warnings
 from datetime import datetime
 
 from _pytest.runner import runtestprotocol
 from croniter import croniter
 
+DEFAULT_RERUN_ATTEMPTS = 1
 PLUGIN_NAME = "dynamicrerun"
 
 
-def _add_dynamic_rerun_flag(parser):
-    help_text = "Set the time to attempt a rerun in using a cron like format ( e.g.: 5 * * * * )"
-
+def _add_dynamic_rerun_schedule_flag(parser):
     group = parser.getgroup(PLUGIN_NAME)
     group.addoption(
-        "--dynamic_rerun_schedule",
+        "--dynamic-rerun-schedule",
         action="store",
         dest="dynamic_rerun_schedule",
         default="* * * * *",
-        help=help_text,
+        help="Set the time to attempt a rerun in using a cron like format ( defaults to '* * * * *' )",
     )
 
-    parser.addini("dynamic_rerun_schedule", help_text)
+    parser.addini("dynamic_rerun_schedule", "default value for --dyamic-rerun-schedule")
 
 
-def _get_dynamic_rerun_arg(item):
+def _add_dynamic_rerun_attempts_flag(parser):
+    group = parser.getgroup(PLUGIN_NAME)
+    group.addoption(
+        "--dynamic-rerun-attempts",
+        action="store",
+        dest="dynamic_rerun_attempts",
+        default=1,
+        help="Set the amount of times reruns should be attempted ( defaults to 1 )",
+    )
+
+    parser.addini(
+        "dynamic_rerun_schedule", "default value for --dynamic-rerun-attempts"
+    )
+
+
+# NOTE: See how we can refactor the _get methods into one method
+#       Or if we even need them, since they're not really doing anything
+#       Also need to check what happens to plugin if installed and nothing is passed
+def _get_dynamic_rerun_schedule_arg(item):
     dynamic_rerun_arg = None
     if item.session.config.option.dynamic_rerun_schedule:
         dynamic_rerun_arg = item.session.config.option.dynamic_rerun_schedule
     return dynamic_rerun_arg
 
 
+def _get_dynamic_rerun_attempts_arg(item):
+    if item.session.config.option.dynamic_rerun_schedule:
+        return item.session.config.option.dynamic_rerun_attempts
+    else:
+        return DEFAULT_RERUN_ATTEMPTS
+
+
 def pytest_addoption(parser):
-    _add_dynamic_rerun_flag(parser)
+    _add_dynamic_rerun_schedule_flag(parser)
+    _add_dynamic_rerun_attempts_flag(parser)
 
 
 def pytest_runtest_protocol(item, nextitem):
     # bail early if a falsey value was given for required args
-    dynamic_rerun_arg = _get_dynamic_rerun_arg(item)
-    if dynamic_rerun_arg is None:
+    dynamic_rerun_schedule_arg = _get_dynamic_rerun_schedule_arg(item)
+    if dynamic_rerun_schedule_arg is None:
         return
 
-    # terminate runs if we've finished all needed runs to prevent inifinite looping
-    # NOTE: for now, we assume one 1 rerun is possible
-    if item.session.num_dynamic_reruns_kicked_off > 1:
-        warnings.warn("Terminating since more dynamic reruns kicked off than requested")
+    dynamic_rerun_attempts_arg = _get_dynamic_rerun_attempts_arg(item)
+    if dynamic_rerun_attempts_arg is None:
+        return
+
+    if item.session.num_dynamic_reruns_kicked_off > dynamic_rerun_attempts_arg:
         return True
 
     item.ihook.pytest_runtest_logstart(nodeid=item.nodeid, location=item.location)
@@ -57,17 +82,12 @@ def pytest_runtest_protocol(item, nextitem):
 
     # if nextitem is None, we have finished running tests. Dynamically rerun any tests that failed
     if nextitem is None:
+        item.session.num_dynamic_reruns_kicked_off += 1
         now_time = datetime.now()
-        time_iterator = croniter(dynamic_rerun_arg, now_time)
+        time_iterator = croniter(dynamic_rerun_schedule_arg, now_time)
 
         time_delta = time_iterator.get_next(datetime) - now_time
         time.sleep(time_delta.seconds)
-
-        warnings.warn(
-            "Sleeping for {} seconds and reruning failed items".format(time_delta)
-        )
-
-        item.session.num_dynamic_reruns_kicked_off += 1
 
         rerun_items = item.session.dynamic_rerun_items
         for i, item in enumerate(rerun_items):
