7d3745ae9cda531ecf5201fabe7002c4ded4a22f
Gleb Nikonorov <gleb.i.nikonorov@gmail.com>
2020-07-25 01:40:57 -0400

    make skeleton for dynamically rerunning tests

--- pytest_dynamicrerun.py
+++ pytest_dynamicrerun.py
@@ -1,6 +1,9 @@
-from _pytest.runner import runtestprotocol
+import time
+import warnings
+from datetime import datetime
 
-# from croniter import croniter - we'll use this to schedule reruns
+from _pytest.runner import runtestprotocol
+from croniter import croniter
 
 PLUGIN_NAME = "dynamicrerun"
 
@@ -10,14 +13,21 @@ def _add_dynamic_rerun_flag(parser):
 
     group = parser.getgroup(PLUGIN_NAME)
     group.addoption(
-        "--rerun_schedule",
+        "--dynamic_rerun_schedule",
         action="store",
-        dest="rerun_schedule",
+        dest="dynamic_rerun_schedule",
         default="* * * * *",
         help=help_text,
     )
 
-    parser.addini("rerun_schedule", help_text)
+    parser.addini("dynamic_rerun_schedule", help_text)
+
+
+def _get_dynamic_rerun_arg(item):
+    dynamic_rerun_arg = None
+    if item.session.config.option.dynamic_rerun_schedule:
+        dynamic_rerun_arg = item.session.config.option.dynamic_rerun_schedule
+    return dynamic_rerun_arg
 
 
 def pytest_addoption(parser):
@@ -25,23 +35,52 @@ def pytest_addoption(parser):
 
 
 def pytest_runtest_protocol(item, nextitem):
+    # bail early if a falsey value was given for required args
+    dynamic_rerun_arg = _get_dynamic_rerun_arg(item)
+    if dynamic_rerun_arg is None:
+        return
+
+    # terminate runs if we've finished all needed runs to prevent inifinite looping
+    # NOTE: for now, we assume one 1 rerun is possible
+    if item.session.num_dynamic_reruns_kicked_off > 1:
+        warnings.warn("Terminating since more dynamic reruns kicked off than requested")
+        return True
+
     item.ihook.pytest_runtest_logstart(nodeid=item.nodeid, location=item.location)
     reports = runtestprotocol(item, nextitem=nextitem, log=False)
     for report in reports:
         if report.failed:
-            # failed test, store it and log as pytest would
-            # NOTE: we can store the failed items like this. Then in sesisonfinsh we can see if anything has failed
-            #       if something has failed then we can schedule a rerun. We can share session state as on line 44
-            item.ihook.pytest_runtest_logreport(report=report)
-            item.session.dynamicrerun = {"failed_item": None}
-            item.session.dynamicrerun["failed_item"] = item
+            item.session.dynamic_rerun_items.append(item)
 
+        item.ihook.pytest_runtest_logreport(report=report)
     item.ihook.pytest_runtest_logfinish(nodeid=item.nodeid, location=item.location)
+
+    # if nextitem is None, we have finished running tests. Dynamically rerun any tests that failed
+    if nextitem is None:
+        now_time = datetime.now()
+        time_iterator = croniter(dynamic_rerun_arg, now_time)
+
+        time_delta = time_iterator.get_next(datetime) - now_time
+        time.sleep(time_delta.seconds)
+
+        warnings.warn(
+            "Sleeping for {} seconds and reruning failed items".format(time_delta)
+        )
+
+        item.session.num_dynamic_reruns_kicked_off += 1
+
+        rerun_items = item.session.dynamic_rerun_items
+        for i, item in enumerate(rerun_items):
+            next_item = rerun_items[i + 1] if i + 1 < len(rerun_items) else None
+            pytest_runtest_protocol(item, next_item)
+
     return True
 
 
-# def pytest_sessionfinish(session, exitstatus):
-#    raise ValueError(session.dynamicrerun)
+def pytest_sessionstart(session):
+    session.dynamic_rerun_items = []
+    session.num_dynamic_reruns_kicked_off = 0
+
 
 # Consider providing a fixture too
 # @pytest.fixture
